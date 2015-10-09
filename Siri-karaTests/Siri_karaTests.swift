4125ffb6a1acdcf5e20c5407d13ae7ae247733fc
esper0328 <esper0328@gmail.com>
2015-10-10 00:09:07 +0900

    Initial Commit

---
+++ Siri-karaTests/Siri_karaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Siri_karaTests.swift
+//  Siri-karaTests
+//
+//  Created by Yohei Kato on 2015/10/10.
+//  Copyright © 2015年 Yohei Kato. All rights reserved.
+//
+
+import XCTest
+@testable import Siri_kara
+
+class Siri_karaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
