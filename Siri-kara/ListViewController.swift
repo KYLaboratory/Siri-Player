feed9f0fd047a34455002f4f03e9e152a3b5331d
honeypon <hogehoge@hogehoge.com>
2015-10-10 12:10:07 +0900

    各画面に対応する.swift作成

---
+++ Siri-kara/ListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ListViewController.swift
+//  Siri-kara
+//
+//  Created by ああ on 2015/10/10.
+//  Copyright © 2015年 Yohei Kato. All rights reserved.
+//
+
+import Foundation
---
+++ Siri-kara/PlayViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PlayViewController.swift
+//  Siri-kara
+//
+//  Created by ああ on 2015/10/10.
+//  Copyright © 2015年 Yohei Kato. All rights reserved.
+//
+
+import Foundation
