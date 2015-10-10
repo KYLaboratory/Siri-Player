88e03191c10be74688e3752c1ab4761767c5fc61
esper0328 <esper0328@gmail.com>
2015-10-10 11:07:14 +0900

    画面遷移

--- Siri-kara/ViewController.swift
+++ Siri-kara/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    var howToMakePlaylist = ["年代", "ジャンル", "曲名しりとり", "？？？"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return howToMakePlaylist.count;  // 1列目の選択肢の数
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return howToMakePlaylist[row]  // 1列目のrow番目に表示する値
+    }
 
 }
 
